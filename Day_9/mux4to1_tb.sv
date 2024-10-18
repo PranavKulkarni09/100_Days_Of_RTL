0a86421ec502b31a5c26e807e00b4ed9c4a27d13
PranavKulkarni09 <mr.pranav.kulkarni.s@gmail.com>
2024-10-17 22:48:05 -0500

    Update mux4to1_tb.sv

--- Day_9/mux4to1_tb.sv
+++ Day_9/mux4to1_tb.sv
@@ -13,6 +13,8 @@ module mux4to1_tb();
     sel = 2'b11; a = 0; b = 0; c = 1; d = 1;
     #10;
     sel = 2'b10; a = 1; b = 1; c = 1; d = 0;
+    #10;
+    $finish();
   end
   initial begin
     $dumpfile("mux4to1.vcd");
