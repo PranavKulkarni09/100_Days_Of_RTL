ab12eb212492e18b1eaccff63078307e8e09d5b1
PranavKulkarni09 <mr.pranav.kulkarni.s@gmail.com>
2024-10-23 15:02:33 -0500

    Update fullAdder_tb.sv

--- Day_13/fullAdder_tb.sv
+++ Day_13/fullAdder_tb.sv
@@ -10,8 +10,6 @@ module fullAdder_tb();
     #10 a = 0; b = 1; c_in = 0;
     #10 a = 0; b = 1; c_in = 1;
     #10 a = 1; b = 0; c_in = 0;
-    #10 a = 1; b = 0; c_in = 1;
-    #10 a = 1; b = 1; c_in = 0;
     #10 a = 1; b = 1; c_in = 1;
   end
 endmodule
